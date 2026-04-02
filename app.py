4377c3c0258a6229a54d6d8abdbc494414b71110
KIMO <steamforkimo@gmail.com>
2026-04-02 14:15:27 +0500

    RAG bot with FastAPI and Streamlit

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+
+API_URL = "http://localhost:8000"
+
+st.set_page_config(page_title="RAG Бот")
+st.title("RAG Бот")
+
+#Проверка на наличие doc_id
+if 'doc_id' not in st.session_state:
+    st.session_state.doc_id = None
+
+#Загрузка документа
+st.header("1. Загрузить документ")
+uploaded_file = st.file_uploader("Выберите текстовый файл", type=["txt"])
+if uploaded_file:
+    text = uploaded_file.read().decode("utf-8")
+    if st.button("Загрузить"):
+        with st.spinner("Загружаю и индексирую..."):
+            response = requests.post(f"{API_URL}/upload", json={"text": text})
+            if response.status_code == 200:
+                data = response.json()
+                st.success(f"Документ загружен! ID: {data['doc_id']}, чанков: {data['chunks_amount']}")
+            else:
+                st.error(f"Ошибка: {response.status_code} - {response.text}")
+    st.session_state.doc_id = response.json()["doc_id"]
+
+#Задать вопрос
+st.header("2. Задать вопрос")
+question = st.text_input("Введите вопрос:")
+if st.button("Спросить"):
+    if st.session_state.doc_id is None:
+        st.warning("Сначала загрузите документ")
+    else:   
+        if not question.strip():
+            st.warning("Введите вопрос")
+        else:
+            with st.spinner("Ищу ответ..."):
+                resp = requests.post(f"{API_URL}/ask", json={"question": question})
+                if resp.status_code == 200:
+                    answer = resp.json()["Ответ"]
+                    st.success("Ответ:")
+                    st.write(answer)
+                else:
+                    st.error(f"Ошибка: {resp.status_code} - {resp.text}")
---
+++ main.py
@@ -0,0 +1,110 @@
+import chromadb
+from chromadb.utils import embedding_functions
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, status
+from chromadb.errors import ChromaError
+from ollama import chat, ChatResponse, ResponseError
+from pydantic import BaseModel
+from typing import Optional
+import uuid
+
+app = FastAPI()
+
+#Создание коллекции ChromDB
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+collection = chroma_client.get_or_create_collection(
+    name="documents",
+    embedding_function=embedding_functions.OllamaEmbeddingFunction(
+        model_name="nomic-embed-text"
+    )
+)
+
+#Создание модели
+class document_upload(BaseModel):
+    text: str
+    doc_id: Optional[str] = None
+    chunk_size: int = 500
+    overlap: int = 50
+
+#Создание функции разделения текста на чанки
+def split_text(text, chunk_size, overlap):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        if end > len(text):
+            end = len(text)
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start = start+(chunk_size-overlap)
+    
+    return chunks
+
+#Добавление эндпоинта для загрузки текста
+@app.post("/upload")
+async def upload(data:document_upload):
+    try:
+        doc_id=data.doc_id
+        if not doc_id or doc_id == "string":
+            doc_id = str(uuid.uuid4())
+        ids = []
+        documents = []
+        metadatas = []
+        chunks = split_text(data.text, data.chunk_size, data.overlap)
+
+        for i, chunk in enumerate(chunks):
+            chunk_id = f"{data.doc_id}_{i}"
+            ids.append(chunk_id)
+            documents.append(chunk)
+            metadatas.append({"doc_id": doc_id, "chunk_index": i})
+        collection.add(ids=ids, documents=documents, metadatas=metadatas)
+        return {
+            "doc_id": doc_id,
+            "chunks_amount": len(chunks)
+        }
+    except ChromaError as e:
+        #Ошибка базы данных
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=f"Ошибка базы данных: {e}")
+    except ValueError as e:
+        #Ошибка валидации данных
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=f"Неверные данные: {e}")
+    except Exception as e:
+        #Любая другая непредвиденная ошибка
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Внутренняя ошибка: {e}")
+
+#Создание модели
+class question(BaseModel):
+    question: str
+    n_results: int = 5
+
+#Добавление эндпоинта для ответа на вопрос
+@app.post("/ask")
+async def asking(data:question):
+    try:
+        response = collection.query( query_texts=[data.question], n_results=data.n_results)
+        chunks = response["documents"][0]
+        if not chunks or chunks == None: #Проверка, если ничего не нашло, то запрос к ИИ не отправляется
+            return {
+                "Ответ": "Ничего не найдено"
+            }
+        else:
+            united_chunks= "\n---\n".join(chunks) 
+            response:   ChatResponse = chat(model='llama3.2', messages=[ #Вопрос к ИИ
+                    {
+                        "role":"user",
+                        "content": f"Используй следующие фрагменты документов для ответа на вопрос.Если в них нет ответа, скажи, что не знаешь. Фрагменты: {united_chunks}. Вопрос: {data.question}"
+                    },
+                ])
+            return {
+                "Вопрос": data.question,
+                "Ответ": response.message.content
+            }
+    #Ошбика на стороне ChromDB
+    except ChromaError as e:
+        raise HTTPException(status_code=503, detail=f"Ошибка векторной базы: {e}")
+    #Ошбика ошбика Ollama
+    except ResponseError as e:
+        raise HTTPException(status_code=503, detail=f"Ошибка модели: {e}")
+    #Отсальные ошибки
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Внутренняя ошибка: {e}")
